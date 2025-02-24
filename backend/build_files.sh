d277bd2c319e93802fb3aee3cc6aa35fd94dfb5b
ramcav <110005362+ramcav@users.noreply.github.com>
2025-02-24 21:00:50 +0100

    Fixed minor bug

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+export PATH="/vercel/.python/bin:$PATH"  # Ensure Python is accessible
+
+# Ensure the correct Python and pip version
+python3 -m venv venv
+source venv/bin/activate
+
 pip install --upgrade pip  # Ensure pip is updated
 pip install --no-cache-dir -r requirements.txt  # Install dependencies without cache
 
