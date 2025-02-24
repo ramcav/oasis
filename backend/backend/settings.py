e9861d4b5f32ed010d7dc6fd7c5f16273fbb722e
ramcav <110005362+ramcav@users.noreply.github.com>
2025-02-24 21:26:52 +0100

    Added env vars and connected to database

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -89,12 +89,25 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+
+if os.getenv("ENV") == "prod" or os.getenv("ENV") == "uat":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.postgresql",
+            "NAME": os.getenv("POSTGRES_DB"),
+            "USER": os.getenv("POSTGRES_USER"),
+            "PASSWORD": os.getenv("POSTGRES_PASSWORD"),
+            "HOST": os.getenv("POSTGRESS_HOST"),
+            "PORT": os.getenv("POSTGRESS_PORT"),
+        }
     }
-}
+else: 
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
