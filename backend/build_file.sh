b4de02d458e0ab737e09e796ac29c7cce55aaa47
ramcav <110005362+ramcav@users.noreply.github.com>
2025-02-24 20:48:56 +0100

    Added static build file

--- backend/build_file.sh
+++ backend/build_file.sh
@@ -1,4 +1,8 @@
-pip install -r requirements.txt
+#!/bin/bash
+
+pip install --upgrade pip  # Ensure pip is updated
+pip install --no-cache-dir -r requirements.txt  # Install dependencies without cache
+
 python manage.py makemigrations
 python manage.py migrate
-python manage.py collectstatic
+python manage.py collectstatic --noinput
