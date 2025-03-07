1e00776d4c330b14cc0e0c72d9edceee70958ac1
ramcav <110005362+ramcav@users.noreply.github.com>
2025-03-07 01:25:22 +0100

    Added id to user serializer

--- backend/user/serializers.py
+++ backend/user/serializers.py
@@ -19,7 +19,7 @@ class UserSerializer(serializers.ModelSerializer):
     
     class Meta:
         model = User
-        fields = ['username', 'password', 'email', 'role']
+        fields = ['id','username', 'password', 'email', 'role']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
