1d948c7350c7815296811d7e280af0574609ed0d
ramcav <110005362+ramcav@users.noreply.github.com>
2025-03-12 17:52:06 +0100

    Updated serializer

--- backend/apartment/serializers.py
+++ backend/apartment/serializers.py
@@ -7,15 +7,26 @@ class CleaningSerializer(serializers.ModelSerializer):
         model = Cleaning
         fields = ['id', 'date', 'status', 'cleaner', 'apartment']
 
+class ArrivalListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Arrival
+        fields = ['id', 'arrival_date', 'departure_date']
+
+class ApartmentListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Apartment
+        fields = ['id', 'name', 'description', 'address']
 
 class ArrivalSerializer(serializers.ModelSerializer):
+    apartment = ApartmentListSerializer(read_only=True)
+    
     class Meta:
         model = Arrival
         fields = ['id', 'apartment', 'arrival_date', 'departure_date']
 
 class ApartmentSerializer(serializers.ModelSerializer):
     cleanings = CleaningSerializer(many=True, read_only=True)
-    arrivals = ArrivalSerializer(many=True, read_only=True)
+    arrivals = ArrivalListSerializer(many=True, read_only=True)
 
     class Meta:
         model = Apartment
